36d15a251ca12b777359be18273d0557873eb8d8
zion-off <ajz317@nyu.edu>
2023-04-20 16:08:30 -0400

    check

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/home.css";
 import "../css/index.css";
 import logo from "../assets/logo.png";
 
 function Home() {
-    
+  const [isHeld, setIsHeld] = useState(false);
+
+  const handleHold = () => {
+    setIsHeld(true);
+  };
+
+  const handleRelease = () => {
+    setIsHeld(false);
+  };
+
   return (
     <div className="home-container">
-      <div className="eight-ball">
+      <div className={`eight-ball ${isHeld ? "held" : ""}`}
+      onMouseDown={handleHold}
+      onMouseUp={handleRelease}
+      onMouseLeave={handleRelease}>
         <img src={logo} alt=""></img>
       </div>
     </div>
