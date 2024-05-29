09f72c1a97e563e35fe2be41851bdc00a335367e
zion-off <ajz317@nyu.edu>
2024-05-30 03:05:27 +0600

    cors issue

--- back-end/app.js
+++ back-end/app.js
@@ -23,12 +23,14 @@ try {
     `Error connecting to MongoDB user account authentication will fail: ${err}`
   );
 }
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", `process.env.FRONT_END_DOMAIN`);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", `process.env.FRONT_END_DOMAIN`);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
 app.use(morgan("dev", { skip: (req, res) => process.env.NODE_ENV === "test" }))
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true })) 
